a080fdc6bd52e024d5d6ebeecd2c92d132db703f
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2018-10-02 10:28:32 +0200

    Added some import settings and a dummy route

--- src/Booking.py
+++ src/Booking.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import mysql.connector
+import os
 
 app = Flask(__name__)
-
+app.debug = True
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/thisIsHowToMakeRoute")
+def thisRouteFunction():
+    return "This is a route I just created"
 
 
 if __name__ == '__main__':
