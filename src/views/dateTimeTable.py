ace6d1045473cb46c8daec8fc9499aef9e740987
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2018-10-05 09:46:37 +0200

    Added some info to dateAndTime page

--- src/views/dateTimeTable.py
+++ src/views/dateTimeTable.py
@@ -5,12 +5,20 @@ dateTimeTable = Blueprint('dateTimeTable', __name__)
 
 @dateTimeTable.route("/dateAndTime")
 def dateAndTime():
-    return render_template('dateTimeTable/index.html')
+    return render_template('dateTimeTable/index.html', restaurant="NameOfRestaurant", 
+    address="Address", telephone="Tel")
     
 
 @dateTimeTable.route('/dateAndTimeConfirmed', methods=["POST"])
 def dateAndTimeConfirmed():
     theDate = request.form["theDate"]
     theTime = request.form["theTime"]
+
+    if(theDate == ""):
+        print("Date not set!")
+        return render_template('dateTimeTable/index.html', err="Date not set!")
+    if(theTime == ""):
+        print("Time not set!")
+        return render_template('dateTimeTable/index.html', err="Time not set!")
 
     return render_template("dateTimeTable/confirmDate.html", theDate=theDate, theTime=theTime)
