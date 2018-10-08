9f48c73c0c95d5d70487eca2f817b6b845c05428
David Galante <davidgalante77@gmail.com>
2018-10-08 23:56:15 +0200

    @DAT210/gruppe1 Integrate docker configuration #10

--- src/__init__.py
+++ src/__init__.py
@@ -12,8 +12,8 @@ app.debug = True
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="yourpwhere",
-    database="yourdbhere"
+    passwd="bankai",
+    database="booking210"
 )
 app.config.from_mapping(DATABASE=mydb,)
 if __name__ == '__main__':
