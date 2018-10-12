810e41753ac0efc1818987050b4ddb7eb22a24ca
Folow <davidgalante77@gmail.com>
2018-10-12 10:47:38 +0200

    Ajax link between pages

---
+++ src/static/js/reservation.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $("#formSearchRestaurant").submit(function(event)
+    {
+        event.preventDefault();
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                $("#main").html(this.responseText);
+                $(".cp-spinner").remove();
+            }
+        };
+        xhttp.open("POST", "/dateAndTime");
+        var formData=$(this).serialize();
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+        xhttp.send(formData);
+    })
+});
--- src/views/searchRestaurant.py
+++ src/views/searchRestaurant.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from jinja2 import TemplateNotFound
 
 searchRestaurant = Blueprint('searchRestaurant', __name__)
+from src import app
 
 @searchRestaurant.route('/')
 def index():
