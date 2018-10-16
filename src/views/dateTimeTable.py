65c57fbb5153cbe39c0c340e243fdba769e5b098
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2018-10-16 10:58:12 +0200

    Users canÃ't choose dates in the past

--- src/views/dateTimeTable.py
+++ src/views/dateTimeTable.py
@@ -1,9 +1,6 @@
 from flask import Blueprint, render_template, request
 import datetime
-from src import app
-from models import Restaurant
-from templatebuild import buildSelectOptions
-from templatebuild import buildTimesButtons
+
 now = datetime.datetime.now()
 
 dateTimeTable = Blueprint('dateTimeTable', __name__)
@@ -15,37 +12,19 @@ def dateAndTime():
     dateNow = now.strftime("%Y-%m-%d")
     print(dateNow)
     theRestaurant = request.form["theRestaurant"]
-    print(theRestaurant)
-    selectedRestaurant=Restaurant.fetchRestaurant(1)
-    mycursor=app.config["DATABASE"].cursor()
-    query="SELECT * FROM period";
-    mycursor.execute(query)
-    periods=mycursor.fetchall()
-    periodsOptions=buildSelectOptions(periods)
-    return render_template('dateTimeTable/chooseDate.html', restaurant=selectedRestaurant,periods=periodsOptions)
 
-
-@dateTimeTable.route('/dateAndTime/time', methods=["POST"])
-def times():
-    period=request.form["period"]
-    mycursor=app.config["DATABASE"].cursor()
-    query="SELECT TIME_FORMAT(time,'%H:%i') FROM time_period WHERE period='"+str(period)+"';"
-    mycursor.execute(query)
-    times=mycursor.fetchall()
-    timesButton=buildTimesButtons(times)
-    return render_template("dateTimeTable/time.html", times=timesButton)
-
+    return render_template('dateTimeTable/chooseDate.html', restaurant=theRestaurant, 
+    address="Stavanger", telephone="555 55 555", dateNow=dateNow)
+    
 
 @dateTimeTable.route('/dateAndTimeConfirmed', methods=["POST"])
 def dateAndTimeConfirmed():
     theDate   = request.form["theDate"]
     theTime   = request.form["theTime"]
     theName   = request.form["theName"]
-    thePeople = request.form["thePeople"]
     thePhone  = request.form["thePhone"]
     theEmail  = request.form["theEmail"]
 
 
-    return render_template("dateTimeTable/confirmDate.html", theDate=theDate, theTime=theTime,
-    theRestaurant=theRestaurant, theName=theName, thePeople=thePeople, thePhone=thePhone, theEmail=theEmail)
-
+    return render_template("dateTimeTable/confirmDate.html", theDate=theDate, theTime=theTime, 
+    theRestaurant=theRestaurant, theName=theName, thePhone=thePhone, theEmail=theEmail)
