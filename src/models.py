9d0a0a7f58de3604fa00fee9d82e50bddd4b6269
Folow <davidgalante77@gmail.com>
2018-10-22 23:17:22 +0200

    add Restaurant class and show dynamic restaurant informations

--- src/models.py
+++ src/models.py
@@ -1,18 +1,16 @@
 from src import app
 class Restaurant :
-    def __init__(self,rid, name, phone, zip, street,latitude,longitude):
+    def __init__(self,rid, name, phone, zip, street):
         self.rid=rid
         self.name = name
         self.phone = phone
         self.zip = zip
         self.street = street
-        self.latitude = latitude
-        self.longitude = longitude
 
     def fetchRestaurant(id):
         mycursor=app.config['DATABASE'].cursor()
         query="SELECT * FROM restaurant WHERE rid="+str(id)+";"
         mycursor.execute(query)
         myresult=mycursor.fetchall()
-        restaurant=Restaurant(myresult[0][0],myresult[0][1],myresult[0][2],myresult[0][3],myresult[0][4],myresult[0][5],myresult[0][6])
+        restaurant=Restaurant(myresult[0][0],myresult[0][1],myresult[0][2],myresult[0][3],myresult[0][4])
         return restaurant
