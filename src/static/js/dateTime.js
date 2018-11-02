ddb54c639e804e39f6c71c38d8fa5a99ee24b578
Tobiasns <35292814+Tobiasns@users.noreply.github.com>
2018-11-02 08:42:01 +0100

    Added AJAX for the pages

--- src/static/js/dateTime.js
+++ src/static/js/dateTime.js
@@ -14,7 +14,40 @@ $(document).ready(function(){
         xhttp.open("POST", "/dateAndTime/time");
         var data=$("#selectPeriod").val();
         var formData="period="+data;
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhttp.send(formData);
     });
+
+    $("#formCheck").submit(function(event)
+    {
+        event.preventDefault();
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                $("#main").html(this.responseText);
+                $(".cp-spinner").remove();
+            }
+        };
+        xhttp.open("POST", "/dateAndTime/checkBooking");
+        var formData=$(this).serialize();
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+        xhttp.send(formData);
+    })
+
+    $("#formConfirmBooking").submit(function(event)
+    {
+        event.preventDefault();
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                $("#main").html(this.responseText);
+                $(".cp-spinner").remove();
+            }
+        };
+        xhttp.open("POST", "/dateAndTime/checkBooking");
+        var formData=$(this).serialize();
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+        xhttp.send(formData);
+    })
+
 });
