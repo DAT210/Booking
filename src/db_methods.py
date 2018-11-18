c95aa5eab29a4eb641095c0df0655158dbc87a43
David Galante <davidgalante77@gmail.com>
2018-11-19 00:14:47 +0100

    Attendance per day and period

--- src/db_methods.py
+++ src/db_methods.py
@@ -64,7 +64,7 @@ def db_get_unavailable_tables(rid, time, date):
     return unvTables
 
 
-def db_insert_booking(rid, tid, date, timeid, cid, add_info):
+def db_insert_booking(rid, tid, date, timeid, cid,people, add_info):
     mycursor = app.config["DATABASE"].cursor()
     try:
         sql = "INSERT INTO booking_info (cid,additional_info) VALUES (%s, %s)"
@@ -72,8 +72,8 @@ def db_insert_booking(rid, tid, date, timeid, cid, add_info):
 
         bid = db_get_bid(cid)
 
-        sql = "INSERT INTO rest_book VALUES (%s, %s, %s, %s, %s)"
-        mycursor.execute(sql, (str(rid), str(bid), str(tid), str(date), str(timeid)))
+        sql = "INSERT INTO rest_book VALUES (%s, %s, %s, %s, %s,%s)"
+        mycursor.execute(sql, (str(rid), str(bid), str(tid), str(date), str(timeid),str(people)))
 
         app.config["DATABASE"].commit()
     except mysql.connector.Error as err:
@@ -117,9 +117,10 @@ def db_insert_full_day(): # Default fullDay
     mycursor = app.config['DATABASE'].cursor()
     try:
         mycursor.execute(
-            "INSERT INTO booking_info VALUES(30,1,'01'),(31,2,'02'),(32,3,'03'),(33,3,'04'),(34,4,'05'),(35,5,'05')")
+            "INSERT INTO booking_info VALUES(30,1,'01'),(31,2,'02'),(32,3,'03'),(33,3,'04'),(34,4,'05'),(50,5,'05'),(51,6,'05'),(52,7,'05'),(53,8,'05'),(54,5,'05'),(55,5,'05'),(56,5,'05'),(57,5,'05'),(58,5,'05'),(59,5,'05'),(60,5,'05'),(61,5,'05'),(62,5,'05')")
+        app.config["DATABASE"].commit()
         mycursor.execute(
-            "INSERT INTO rest_book VALUES(1,30,'01','2018-11-17',1),(1,31,'02','2018-11-17',1),(1,32,'03','2018-11-17',1),(1,33,'04','2018-11-17',1),(1,34,'05','2018-11-17',1)")
+            "INSERT INTO rest_book VALUES(1,30,'01','2018-11-17',1,2),(1,31,'02','2018-11-17',1,2),(1,32,'03','2018-11-17',1,3),(1,33,'04','2018-11-17',1,3),(1,34,'05','2018-11-17',1,3),(1,50,'05','2018-11-20',1,6),(1,51,'05','2018-11-20',1,4),(1,52,'05','2018-11-20',1,4),(1,53,'05','2018-11-20',1,6),(1,54,'05','2018-11-20',1,3),(1,55,'05','2018-11-20',1,3),(1,56,'05','2018-11-20',1,3),(1,57,'05','2018-11-21',1,8),(1,58,'05','2018-11-21',1,8),(1,59,'05','2018-11-21',1,8),(1,60,'05','2018-11-21',1,8),(1,61,'05','2018-11-21',1,8),(1,62,'05','2018-11-21',1,3)")
         app.config["DATABASE"].commit()
     except mysql.connector.Error as err:
         print("Error: {}".format(err.msg))
@@ -130,8 +131,8 @@ def db_insert_full_day(): # Default fullDay
 def db_delete_full_day():  # Remove default fullDay
     mycursor = app.config['DATABASE'].cursor()
     try:
-        mycursor.execute("DELETE FROM rest_book WHERE rest_book.bid IN(30,31,32,33,34,35)")
-        mycursor.execute("DELETE FROM booking_info WHERE booking_info.bid IN(30,31,32,33,34,35)")
+        mycursor.execute("DELETE FROM rest_book WHERE rest_book.bid IN(30,31,32,33,34,50,51,52,53,54,55,56,57,58,59,60,61,62)")
+        mycursor.execute("DELETE FROM booking_info WHERE booking_info.bid IN(30,31,32,33,34,50,51,52,53,54,55,56,57,58,59,60,61,62)")
         app.config["DATABASE"].commit()
     except mysql.connector.Error as err:
         print("Error: {}".format(err.msg))
@@ -144,6 +145,20 @@ def db_get_times_from_period(period):
         sql = "SELECT time_period.timeid FROM time_period WHERE period=%s"
         mycursor.execute(sql, (str(period),))
         return mycursor.fetchall()
+    except mysql.connector.Error as err:
+        print("Error: {}".format(err.msg))
+    finally:
+        mycursor.close()
+
+def db_get_attendance(date,period):
+    mycursor = app.config['DATABASE'].cursor()
+    try:
+        sql = "SELECT SUM(people) FROM rest_book JOIN time_period on rest_book.timeid=time_period.timeid WHERE time_period.period=%s AND rest_book.date=%s"
+        mycursor.execute(sql, (str(period),str(date)))
+        sum=mycursor.fetchall()[0][0]
+        if str(sum) !="None":
+            return int(sum)
+        return 0
     except mysql.connector.Error as err:
         print("Error: {}".format(err.msg))
     finally:
--- test/test_booking.py
+++ test/test_booking.py
@@ -5,6 +5,7 @@ import datetime
 from src import app
 from src import read_db_config
 from src.views.dateTimeTable import calculCalendarWeeks, dayNumberCalendar,isDayDisabled,isTimeDisabled
+from src.db_methods import db_get_attendance
 
 @pytest.fixture
 def client():
@@ -43,7 +44,7 @@ def test_isDayDisabled(client):
     mycursor = app.config['DATABASE'].cursor()
     mycursor.execute("INSERT INTO booking_info VALUES(30,1,'01'),(31,2,'02'),(32,3,'03'),(33,3,'04'),(34,4,'05'),(35,5,'05')")
     app.config["DATABASE"].commit()
-    mycursor.execute("INSERT INTO rest_book VALUES(1,30,'01','2018-11-13',5),(1,31,'02','2018-11-13',5),(1,32,'03','2018-11-13',5),(1,33,'04','2018-11-13',5),(1,34,'05','2018-11-13',5)")
+    mycursor.execute("INSERT INTO rest_book VALUES(1,30,'01','2018-11-13',5,2),(1,31,'02','2018-11-13',5,3),(1,32,'03','2018-11-13',5,4),(1,33,'04','2018-11-13',5,6),(1,34,'05','2018-11-13',5,4)")
     app.config["DATABASE"].commit()
     disabled=isDayDisabled(["01","02","03","04","05"],"2018-11-13","lunch")
     mycursor.execute("DELETE FROM rest_book WHERE rest_book.bid IN(30,31,32,33,34,35)")
@@ -56,7 +57,7 @@ def test_isTimeDisabled(client):
     mycursor = app.config['DATABASE'].cursor()
     mycursor.execute("INSERT INTO booking_info VALUES(30,1,'01'),(31,2,'02'),(32,3,'03'),(33,3,'04'),(34,4,'05'),(35,5,'05')")
     app.config["DATABASE"].commit()
-    mycursor.execute("INSERT INTO rest_book VALUES(1,30,'01','2018-11-18',5),(1,31,'02','2018-11-18',5),(1,32,'03','2018-11-18',5),(1,33,'04','2018-11-18',5),(1,34,'05','2018-11-18',5)")
+    mycursor.execute("INSERT INTO rest_book VALUES(1,30,'01','2018-11-18',5,3),(1,31,'02','2018-11-18',5,2),(1,32,'03','2018-11-18',5,1),(1,33,'04','2018-11-18',5,2),(1,34,'05','2018-11-18',5,4)")
     app.config["DATABASE"].commit()
     disabled=isTimeDisabled("2018-11-18",[9],"lunch")
     mycursor.execute("DELETE FROM rest_book WHERE rest_book.bid IN(30,31,32,33,34,35)")
@@ -64,3 +65,16 @@ def test_isTimeDisabled(client):
     mycursor.execute("DELETE FROM booking_info WHERE booking_info.bid IN(30,31,32,33,34,35)")
     app.config["DATABASE"].commit()
     assert disabled
+
+def test_attendance(client):
+    mycursor = app.config['DATABASE'].cursor()
+    mycursor.execute("INSERT INTO booking_info VALUES(30,1,'01'),(31,2,'02'),(32,3,'03'),(33,3,'04'),(34,4,'05'),(35,5,'05')")
+    app.config["DATABASE"].commit()
+    mycursor.execute("INSERT INTO rest_book VALUES(1,30,'01','2018-11-18',5,3),(1,31,'02','2018-11-18',5,2),(1,32,'03','2018-11-18',5,1),(1,33,'04','2018-11-18',5,2),(1,34,'05','2018-11-18',5,4)")
+    app.config["DATABASE"].commit()
+    attendance=db_get_attendance("2018-11-18","lunch")
+    mycursor.execute("DELETE FROM rest_book WHERE rest_book.bid IN(30,31,32,33,34,35)")
+    app.config["DATABASE"].commit()
+    mycursor.execute("DELETE FROM booking_info WHERE booking_info.bid IN(30,31,32,33,34,35)")
+    app.config["DATABASE"].commit()
+    assert attendance==12
